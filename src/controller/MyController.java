2a1c325427197e7ee1259dfd460cac1ca65372ae
Krista <krista.puke@gmail.com>
2014-11-28 01:19:58 +0100

    Krista: forgo to save :/

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -246,7 +246,6 @@ public class MyController implements Initializable {
 		@FXML private ImageView btnpicLock;
 		@FXML private Button btnLock;
 		@FXML public void lockDesctop(MouseEvent event) throws Exception{
-			System.out.print("Lock");	
 			String lockDesctop = "C:\\Windows\\System32\\rundll32.exe user32.dll,LockWorkStation";
 			Runtime.getRuntime().exec(lockDesctop);
 		}
@@ -255,8 +254,7 @@ public class MyController implements Initializable {
 		//ShDown
 		@FXML private ImageView btnpicDown;
 		@FXML private Button btnDown;
-		@FXML public void openDown(MouseEvent event) throws Exception{
-			System.out.print("CMD");	
+		@FXML public void openDown(MouseEvent event) throws Exception{	
 			String lockDesctop = "C:\\Windows\\System32\\rundll32.exe user32.dll,LockWorkStation";
 			Runtime.getRuntime().exec(lockDesctop);
 		}
